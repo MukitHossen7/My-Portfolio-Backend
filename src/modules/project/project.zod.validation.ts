f0d5eb4ee8a4c8266b56ecf4c73205db3861863b
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-28 22:21:54 +0600

    project zod schema is created

---
+++ src/modules/project/project.zod.validation.ts
@@ -0,0 +1,39 @@
+import z from "zod";
+
+export const createProjectZodSchema = z.object({
+  ownerId: z.number().optional(),
+  title: z
+    .string({ required_error: "Title is required" })
+    .min(3, "Title must be at least 3 characters"),
+  description: z
+    .string({ required_error: "Description is required" })
+    .min(10, "Description must be at least 10 characters"),
+  thumbnail: z.string({ required_error: "Thumbnail is required" }),
+  frontendRepoUrl: z
+    .string()
+    .url("Frontend repo must be a valid URL")
+    .optional(),
+  backendRepoUrl: z.string().url("Backend repo must be a valid URL").optional(),
+  liveUrl: z.string().url("Live URL must be a valid URL").optional(),
+  features: z.array(z.string()).optional(),
+});
+
+export const updateProjectZodSchema = z.object({
+  ownerId: z.number().optional(),
+  title: z
+    .string({ required_error: "Title is required" })
+    .min(3, "Title must be at least 3 characters")
+    .optional(),
+  description: z
+    .string({ required_error: "Description is required" })
+    .min(10, "Description must be at least 10 characters")
+    .optional(),
+  thumbnail: z.string({ required_error: "Thumbnail is required" }).optional(),
+  frontendRepoUrl: z
+    .string()
+    .url("Frontend repo must be a valid URL")
+    .optional(),
+  backendRepoUrl: z.string().url("Backend repo must be a valid URL").optional(),
+  liveUrl: z.string().url("Live URL must be a valid URL").optional(),
+  features: z.array(z.string()).optional(),
+});
